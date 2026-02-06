e0a6ea0657eb2ed818153713e3a6a7015ebe888b
Robert Mueller <rm3942721@gmail.com>
2026-02-06 08:07:47 +0100

    viewentrymodal activate on Details button

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -1,36 +1,10 @@
-import { useState } from "react";
 import EntryDetails from "./EntryDetails";
 
-import ViewEntryModal from "./ViewEntryModal";
-
-/*
-const EntryCard = ({entry}) => {
-    const [open, setOpen] = useState(false);
-    return (
-    <>
-        <EntryDetails entry={entry} constrained={true} disabled={false} onDetails={() => setOpen(true)}/>
-        {open && (
-            <ViewEntryModal onClose={() => setOpen(false)}>
-                <EntryDetails entry={entry} constrained={false} disabled={true} />
-            </ViewEntryModal>
-        )}
-    </>
-)};
-*/
-
 const EntryCard = ({entry, onClick }) => {
-    const [open, setOpen] = useState(false);
     return (
-    <div className="bg-white rounded-lg shadow-md p-4 cursor-pointer" onClick={onClick}>
-        <EntryDetails entry={entry} constrained={true} disabled={false} onDetails={() => setOpen(true)}/>
-        {open && (
-        <ViewEntryModal onClose={() => setOpen(false)}>
-        <EntryDetails entry={entry} constrained={false} disabled={true} />
-        </ViewEntryModal>
-        )}
+    <div className="bg-white rounded-lg shadow-md p-4 cursor-pointer">
+        <EntryDetails entry={entry} constrained={true} disabled={false} onDetails={(event) => onClick(entry)}/>
     </div>
 )};
 
 export default EntryCard;
-
-
